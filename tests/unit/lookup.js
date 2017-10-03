6d9cf2835f20b7400653e05bc3d83bf7eeeacd9c
Steven Kalt <kalt.steven@gmail.com>
2017-10-03 18:37:51 -0400

    first unit test added

---
+++ tests/unit/lookup.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+// from src/js/events.js; not bothering to import it
+const lookup = (obj, ...path) => {
+  if (!obj || !path.length ) return undefined;
+  let [next, ...rest] = path;
+  return (rest.length) ? lookup(obj[next], ...rest):  obj[next];
+};
+
+describe('lookup', function(){
+  it('returns correctly on an empty object', ()=>{
+    let a = {};
+    assert.equal(lookup(a, 'b', 'c'), undefined);
+  });
+  it('returns correctly without any path', ()=>{
+    let a = {a:{b:1}};
+    assert.equal(lookup(a), undefined);
+  });
+  it('returns correctly', ()=>{
+    let a = {a:{b:1}};
+    assert.ok(lookup(a, 'a', 'b') == 1);
+    assert.ok(lookup(a, 'a', 'foo') == undefined);
+  });
+})
