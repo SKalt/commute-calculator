b543c1316d0de2c5c9b11451c422ee497b59ee2f
Steven Kalt <kalt.steven@gmail.com>
2017-10-04 17:32:39 -0400

    brainstorming

--- tests/acceptance/add_candidate_commutes_test.js
+++ tests/acceptance/add_candidate_commutes_test.js
@@ -2,5 +2,11 @@
 Feature('Add candidate commutes');
 
 Scenario('test something', (I) => {
-
+  I.amOnPage('/');
+  I.click('#add-commute.mode');
+  // need a method to test for
+  I.moveCursorTo(1,1); //TODO: get demo pixel coords
+  I.click();
+  I.moveCursorTo(1,1); //TODO: get demo pixel coords
+  I.click();
 });
