8cc86ec5aec36fb610ec0f0a1bb8242636d838c6
Steven Kalt <kalt.steven@gmail.com>
2017-10-04 12:30:49 -0400

    added geocoder

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-/* global maboxgl, ENV, document */
+/* global mapboxgl, document */
 import select from './shorthand.js';
 import {debug} from './debug.js';
 import {createStore, combineReducers} from 'redux';
 import {setupEvents} from './events.js';
- import '../css/style.css';
+import '../css/style.css';
 
 import {mapModes} from './reducers/map-modes.js';
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -20,5 +20,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
     center: [-84.388, 33.749], // starting position [lng, lat]
     zoom: 9 // starting zoom
   });
+  let geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    placeholder: 'Search by address'
+  });
+  map.addControl(geocoder);
   setupEvents(store, events);
 });
